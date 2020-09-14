1ea29ae388e4d5de3c1a38708a5afd0beebf786c
Shika <3mo.shika.10@gmail.com>
2020-09-14 17:59:19 +0200

    Decreasing order

---
+++ Java/Sorting/InsertionSort-Decreasing-.java
@@ -0,0 +1,20 @@
+package Java.Sorting;
+
+public class InsertionSortDecreasing {
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 2, 4, 6, 1, 3 };
+        for (int j = 1; j < nums.length; j++) {
+            int key = nums[j];
+            int i = j - 1;
+            while (i > -1 && nums[i] < key) {
+                nums[i + 1] = nums[i];
+                i--;
+            }
+            nums[i + 1] = key;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
