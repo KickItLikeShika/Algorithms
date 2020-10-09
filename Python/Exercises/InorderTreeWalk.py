bd5ab8dd90ebb80e96d6a68b9830ef14d24ddf12
Shika <3mo.shika.10@gmail.com>
2020-10-09 14:17:53 +0200

    Inorder tree walk solved

---
+++ Python/Exercises/InorderTreeWalk.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left == None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            if data > self.data:
+                if self.right == None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+
+        else:
+            self.key = data
+
+    def inorder_tree_walk(self, root):
+        if root:
+            self.inorder_tree_walk(root.left)
+            print(root.data)
+            self.inorder_tree_walk(root.right)
+
+
+if __name__ == '__main__':
+    root = Node(27)
+    root.insert(14)
+    root.insert(35)
+    root.insert(10)
+    root.insert(19)
+    root.insert(31)
+    root.insert(42)
+    root.inorder_tree_walk(root)
